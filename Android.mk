1e6583c00f53ead685b798e30fdc488ac21ced25
drohan898 <drohan940@gmail.com>
2017-08-05 11:24:55 +0530

    Fix Missing headers
    Signed-off-by: drohan898 <drohan940@gmail.com>

--- Android.mk
+++ Android.mk
@@ -2,10 +2,8 @@ QCOM_MEDIA_ROOT := $(call my-dir)
 $(warning target list is : $(MSM_VIDC_TARGET_LIST)) 
  
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
 include $(QCOM_MEDIA_ROOT)/mm-core/Android.mk
 include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
-endif
 
 ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true) 
 ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
